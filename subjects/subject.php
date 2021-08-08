c19f70f2a419c6005dd8479fc30c0686a8998ae2
AzrilSyamin <azril@azrilsyamin.com>
2021-08-09 00:23:19 +0800

    susun code dan ubah display cikgu_id => nama_cikgu , nama table No Cikgu => Nama Cikgu file => subjects/subject.php

--- subjects/subject.php
+++ subjects/subject.php
@@ -1,40 +1,41 @@
 <?php include_once "../_header.php"; ?>
 <?php
 
-$subjects= query("SELECT * FROM tb_pelajaran
-                 ");
+$subjects = query("SELECT * FROM tb_cikgu
+                JOIN tb_pelajaran
+                ON tb_cikgu.id = tb_pelajaran.cikgu_id");
 ?>
 
-                    <!-- Page Heading -->
-                    <h1>List Of Subjects</h1>
-                        <div class="row">
-                            <!-- Awal Table  -->
-                            <div class="col-12">
-                                <table class="table shadow">
-                                <thead class="thead-dark">
-                                    <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Name Pelajaran</th>
-                                    <th scope="col">No Cikgu</th>
-                                    <th scope="col">Action</th>
-                                </thead>
-                                <?php $i=1; foreach($subjects as $subject):?>
-                                <tbody>
-                                    <tr>
-                                    <th scope="row"><?= $i++;?></th>
-                                    <td><?= $subject["mata_pelajaran"];?></td>
-                                    <td><?= $subject["cikgu_id"];?></td>
-                      <td>
-                        
-                        <a class="badge badge-warning" href="edit.php?id=<?= $subject["id"];?>"><i class="bi bi-pencil-square"></i></a>
-                        
-                        <a class="badge badge-danger" href="del.php?id=<?= $subject["id"];?>" onclick="return confirm('Are You Sure Want To Delete?');"><i class="bi bi-trash"></i></a>
-                      </td>
-                                    </tr>
-                                </tbody>
-                                <?php endforeach;?>
-                                </table>
-                            </div>
-                            <!-- Akhir Table  -->
-                        </div>
+<!-- Page Heading -->
+<h1>List Of Subjects</h1>
+<div class="row">
+    <!-- Awal Table  -->
+    <div class="col-12">
+        <table class="table shadow">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name Mata Pelajaran</th>
+                    <th scope="col">Nama Cikgu</th>
+                    <th scope="col">Action</th>
+            </thead>
+            <?php $i = 1;
+            foreach ($subjects as $subject) : ?>
+                <tbody>
+                    <tr>
+                        <th scope="row"><?= $i++; ?></th>
+                        <td><?= $subject["mata_pelajaran"]; ?></td>
+                        <td><?= $subject["nama_cikgu"]; ?></td>
+                        <td>
+                            <a class="badge badge-warning" href="edit.php?id=<?= $subject["id"]; ?>"><i class="bi bi-pencil-square"></i></a>
+
+                            <a class="badge badge-danger" href="del.php?id=<?= $subject["id"]; ?>" onclick="return confirm('Are You Sure Want To Delete?');"><i class="bi bi-trash"></i></a>
+                        </td>
+                    </tr>
+                </tbody>
+            <?php endforeach; ?>
+        </table>
+    </div>
+    <!-- Akhir Table  -->
+</div>
 <?php include_once "../_footer.php"; ?>
