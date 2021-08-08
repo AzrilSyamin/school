9253584cb25fe36cd44d7b85be70c29f15bea1dd
AzrilSyamin <azril@azrilsyamin.com>
2021-08-08 23:15:50 +0800

    susun code file teachers/add.php, ubah input number => nama cikgu file students/add.php

--- subjects/add.php
+++ subjects/add.php
@@ -3,45 +3,51 @@
 
 if (isset($_POST["submit"])) {
     if (add_subjects($_POST) > 0) {
-      echo "<script>
+        echo "<script>
       
       document.location.href='../subjects/subject.php';
       </script>
       ";
     } else {
-      var_dump(mysqli_error($con));
-       $error=true;
-
-      
+        $error = true;
     }
-  }
+}
 ?>
 
-                    <!-- Page Heading -->
-                    <h1>Add New Subjects</h1>
-                        <div class="row justify-content-center">
-                            <!-- Awal Form  -->
-                            <div class="col-12 col-md-6 p-4 shadow">
-                            <?php if(isset($error)):?>
-                                <div class="alert alert-danger" role="alert">
-                                    <p>Subjects is <b>not added!</b>
-                                    <br> Please again try later...</p>
-                                </div>
-                            <?php endif;?>
-                                <form action="" method="POST">
-                                    <div class="form-group">
-                                        <label for="mata">Nama Mata Pelajaran :</label>
-                                        <input type="text" class="form-control" name="mata" id="mata " autofocus required>
-                                    </div>
-                      
-                      <div class="form-group">
-                                        <label for="idcikgu">No Cikgu :</label>
-                                        <input type="number" class="form-control" name="idcikgu" id="idcikgu " required>
-                                    </div>
-                     
-                                    <button type="submit" class="btn btn-primary" name="submit">Add New Subject</button>
-                                </form>
-                            </div>
-                            <!-- Akhir Form  -->
-                        </div>
+<!-- Page Heading -->
+<h1>Add New Subjects</h1>
+<div class="row justify-content-center">
+    <!-- Awal Form  -->
+    <div class="col-12 col-md-6 p-4 shadow">
+        <?php if (isset($error)) : ?>
+            <div class="alert alert-danger" role="alert">
+                <p>Subjects is <b>not added!</b>
+                    <br> Please again try later...
+                </p>
+            </div>
+        <?php endif; ?>
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="mata">Nama Mata Pelajaran :</label>
+                <input type="text" class="form-control" name="mata" id="mata " autofocus required>
+            </div>
+
+            <div class="form-group">
+                <label for="idcikgu">Name Cikgu :</label>
+
+                <select name="idcikgu" id="idcikgu" class="form-control">
+                    <option selected>Choose...</option>
+                    <?php
+                    $teachers = query("SELECT * FROM tb_cikgu");
+                    foreach ($teachers as $teacher) : ?>
+                        <option value="<?= $teacher["id"]; ?>"><?= $teacher["nama_cikgu"]; ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+
+            <button type="submit" class="btn btn-primary" name="submit">Add New Subject</button>
+        </form>
+    </div>
+    <!-- Akhir Form  -->
+</div>
 <?php include_once "../_footer.php"; ?>
--- teachers/add.php
+++ teachers/add.php
@@ -1,4 +1,4 @@
-<?php include_once ("../_header.php"); ?>
+<?php include_once("../_header.php"); ?>
 <?php
 
 if (isset($_POST["submit"])) {
@@ -9,45 +9,46 @@ if (isset($_POST["submit"])) {
         </script>
         ";
     } else {
-        $error=true;
+        $error = true;
     }
 }
 
 ?>
-                    <!-- Page Heading -->
-                    <h1>Add New Teachers</h1>
-                        <div class="row justify-content-center">
-                            <!-- Awal Form  -->
-                            <div class="col-12 col-md-6 p-4 shadow">
-                            <?php if(isset($error)):?>
-                                <div class="alert alert-danger" role="alert">
-                                    <p>Teacher is <b>not added!</b>
-                                    <br> Please again try later...</p>
-                                </div>
-                            <?php endif;?>
-                                <form action="" method="POST">
-                                    <div class="form-group">
-                                        <label for="nama">Nama :</label>
-                                        <input type="text" class="form-control" name="nama" id="nama" required autofocus>
-                                    </div>
+<!-- Page Heading -->
+<h1>Add New Teachers</h1>
+<div class="row justify-content-center">
+    <!-- Awal Form  -->
+    <div class="col-12 col-md-6 p-4 shadow">
+        <?php if (isset($error)) : ?>
+            <div class="alert alert-danger" role="alert">
+                <p>Teacher is <b>not added!</b>
+                    <br> Please again try later...
+                </p>
+            </div>
+        <?php endif; ?>
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="nama">Nama :</label>
+                <input type="text" class="form-control" name="nama" id="nama" required autofocus>
+            </div>
 
-                                    <div class="form-group">
-                                        <label for="umur">Umur :</label>
-                                        <input type="number" class="form-control" name="umur" id="umur" required>
-                                    </div>
-                                    
-                                    <div class="form-group">
-                                        <label for="jantina">Jantina :</label>
-                                        <select name="jantina" id="jantina" class="form-control">
-                                        <option selected>Choose...</option>
-                                            <option>Lelaki</option>
-                                            <option>Perempuan</option>
-                                        </select>
-                                    </div>
+            <div class="form-group">
+                <label for="umur">Umur :</label>
+                <input type="number" class="form-control" name="umur" id="umur" required>
+            </div>
 
-                                    <button type="submit" class="btn btn-primary" name="submit">Add New Teacher</button>
-                                </form>
-                            </div>
-                            <!-- Akhir Form  -->
-                        </div>
-<?php include_once ("../_footer.php"); ?>                
+            <div class="form-group">
+                <label for="jantina">Jantina :</label>
+                <select name="jantina" id="jantina" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>Lelaki</option>
+                    <option>Perempuan</option>
+                </select>
+            </div>
+
+            <button type="submit" class="btn btn-primary" name="submit">Add New Teacher</button>
+        </form>
+    </div>
+    <!-- Akhir Form  -->
+</div>
+<?php include_once("../_footer.php"); ?>
