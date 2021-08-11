743968f195906c96da572b4528d90de702bb222b
AzrilSyamin <azril@azrilsyamin.com>
2021-08-11 23:46:46 +0800

    ubah ui dalam page forget

--- auth/forget.php
+++ auth/forget.php
@@ -20,13 +20,13 @@
   <div class="login-box">
     <div class="card card-outline card-primary">
       <div class="card-header text-center">
-        <a href="../_asset/index2.html" class="h1"><b>Admin</b>LTE</a>
+        <a href="/" class="h1"><b>Data </b>Sekolah</a>
       </div>
       <div class="card-body">
         <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
-        <form action="recover-password.html" method="post">
+        <form action="" method="post">
           <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email">
+            <input type="email" class="form-control" placeholder="Email" name="email">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -35,13 +35,13 @@
           </div>
           <div class="row">
             <div class="col-12">
-              <button type="submit" class="btn btn-primary btn-block">Request new password</button>
+              <button type="submit" class="btn btn-primary btn-block" name="submit">Request new password</button>
             </div>
             <!-- /.col -->
           </div>
         </form>
         <p class="mt-3 mb-1">
-          <a href="login.html">Login</a>
+          <a href="login.php">Login</a>
         </p>
       </div>
       <!-- /.login-card-body -->
