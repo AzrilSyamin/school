d027c3570ca2dac7dcd6ee1ad9d9f27ae0310f5d
AzrilSyamin <azril@azrilsyamin.com>
2021-08-11 23:24:24 +0800

    ubah ui dalam page login

--- auth/login.php
+++ auth/login.php
@@ -21,14 +21,14 @@
     <!-- /.login-logo -->
     <div class="card card-outline card-primary">
       <div class="card-header text-center">
-        <a href="../_asset/index2.html" class="h1"><b>Admin</b>LTE</a>
+        <a href="../_asset/index2.html" class="h1"><b>Data </b>Sekolah</a>
       </div>
       <div class="card-body">
         <p class="login-box-msg">Sign in to start your session</p>
 
         <form action="../_asset/index3.html" method="post">
           <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email">
+            <input type="email" class="form-control" placeholder="Email" name="email">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -36,7 +36,7 @@
             </div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password">
+            <input type="password" class="form-control" placeholder="Password" name="password">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
@@ -54,27 +54,27 @@
             </div>
             <!-- /.col -->
             <div class="col-4">
-              <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+              <button type="submit" class="btn btn-primary btn-block" name="submit">Sign In</button>
             </div>
             <!-- /.col -->
           </div>
         </form>
 
-        <div class="social-auth-links text-center mt-2 mb-3">
+        <!-- <div class="social-auth-links text-center mt-2 mb-3">
           <a href="#" class="btn btn-block btn-primary">
             <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
           </a>
           <a href="#" class="btn btn-block btn-danger">
             <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
           </a>
-        </div>
+        </div> -->
         <!-- /.social-auth-links -->
 
         <p class="mb-1">
-          <a href="forgot-password.html">I forgot my password</a>
+          <a href="forget.php">I forgot my password</a>
         </p>
         <p class="mb-0">
-          <a href="register.html" class="text-center">Register a new membership</a>
+          <a href="reg.php" class="text-center">Register a new membership</a>
         </p>
       </div>
       <!-- /.card-body -->
