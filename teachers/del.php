0f39a730da8a2f148f6a3d8f51b6fa9a27456f97
AzrilSyamin <azril@azrilsyamin.com>
2021-09-23 20:38:00 +0800

    update elakkan orang delete data sebelum login dan role moderator tak boleh delete

--- teachers/del.php
+++ teachers/del.php
@@ -1,5 +1,17 @@
 <?php
 include_once("../function.php");
+
+if (isset($_SESSION["admin"])) {
+  $login = $_SESSION["admin"];
+}
+
+if (!isset($login)) {
+  echo "<script>
+window.location='/'
+</script>";
+  exit;
+}
+
 $id = $_GET["id"];
 
 mysqli_query($con, "DELETE FROM tb_user WHERE id = $id") or die(mysqli_error($con));
