131168dc53ad1db3cb9adb83752642111e3fcf45
azrilsyamin <azril@azrilsyamin.com>
2023-03-24 02:42:52 +0800

    ubah function.php, tambah file create.php dan setup.php

---
+++ setup.php
@@ -0,0 +1,59 @@
+<?php
+echo "<h1>Ini file setup</h1>";
+
+if (isset($_POST["setup"])) {
+  require_once "create.php";
+  create_config();
+  require_once "config.php";
+
+  $con = mysqli_connect($host, $user, $pass);
+  if (!$con) {
+    echo "<p style=\"color:red;text-align:center\">Failed To Connect</p>" . mysqli_connect_error();
+  }
+
+  $create_db = "CREATE DATABASE IF NOT EXISTS $db_name";
+  if (!mysqli_query($con, $create_db) === true) {
+    echo "<p style=\"color:red;text-align:center\">Failed To Create Database :</p>" . mysqli_error($con);
+  } else {
+    $con = mysqli_connect($host, $user, $pass, $db_name) or die(mysqli_error($con));
+    create_tabel();
+    insert_table_data();
+  }
+}
+?>
+
+<form action="" method="post">
+
+  <div>
+    <label>
+      Hostname
+      <input type="text" name="hostname">
+    </label>
+  </div>
+  <div>
+    <label>
+      Database Name
+      <input type="text" name="db_name">
+    </label>
+  </div>
+  <div>
+    <label>
+      Username
+      <input type="text" name="username">
+    </label>
+  </div>
+  <div>
+    <label>
+      Password
+      <input type="password" name="password">
+    </label>
+  </div>
+  <div>
+    <label>
+      Website URL
+      <input type="url" name="webUrl">
+    </label>
+  </div>
+
+  <button type="submit" name="setup">Next</button>
+</form>
